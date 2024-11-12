3f80fd1e775c3a467b674f2c895d5f59a6971cee
Liam Nguyen <jimmynlnguyen@gmail.com>
2024-11-12 19:39:55 +0000

    json store and read

--- ts/data.ts
+++ ts/data.ts
@@ -11,3 +11,20 @@ const data: Data = {
   editing: null,
   nextEntryId: 1,
 };
+
+const dataJSON = JSON.stringify(data);
+
+function storeData(key: string, data: any): any {
+  localStorage.setItemItem(key, dataJSON);
+  return data;
+}
+console.log(storeData);
+
+function readData(key: string): any {
+  const getJSON = localStorage.getItem(key);
+  if (getJSON) {
+    return JSON.parse(dataJSON);
+  } else {
+    return 'Error';
+  }
+}
